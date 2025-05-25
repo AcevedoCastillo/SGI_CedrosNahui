7b37397036f786132027c96558fa13aed41ba623
Kevin Grande <kevinbussines2020@gmail.com>
2025-05-25 00:57:17 -0600

    feat: ✨ Add User DTO

---
+++ CedrosNahuizalquenos/CedrosNahuizalquenos/DTOs/UsuarioDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace CedrosNahuizalquenos.DTOs
+{
+    public class UsuarioDTO
+    {
+        public int UsuarioId { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string Correo { get; set; } = null!;
+        public string Contrasena { get; set; } = null!;
+        public int Rol { get; set; }
+        public bool Activo { get; set; }
+        public DateTime FechaRegistro { get; set; }
+    }
+}
