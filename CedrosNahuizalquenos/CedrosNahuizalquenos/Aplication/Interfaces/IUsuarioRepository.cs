71fcdcf018b299c1d4a19897639e4a7f2b36fe73
Kevin Grande <kevinbussines2020@gmail.com>
2025-05-25 00:58:15 -0600

    feat: ✨ Add User Repository

---
+++ CedrosNahuizalquenos/CedrosNahuizalquenos/Aplication/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,13 @@
+﻿using CedrosNahuizalquenos.Domain.Entities;
+
+namespace CedrosNahuizalquenos.Aplication.Interfaces
+{
+    public interface IUsuarioRepository
+    {
+        Task<IEnumerable<Usuario>> GetAllAsync();
+        Task<Usuario?> GetByIdAsync(int id);
+        Task AddAsync(Usuario usuario);
+        Task UpdateAsync(Usuario usuario);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ CedrosNahuizalquenos/CedrosNahuizalquenos/Infrastructure/Services/UsuarioRepository.cs
@@ -0,0 +1,43 @@
+﻿using CedrosNahuizalquenos.Aplication.Interfaces;   
+using CedrosNahuizalquenos.Domain.Entities;
+using CedrosNahuizalquenos.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CedrosNahuizalquenos.Infrastructure.Services
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public UsuarioRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Usuario>> GetAllAsync()
+        {
+            return await _context.Usuarios.ToListAsync();
+        }
+        public async Task<Usuario?> GetByIdAsync(int id)
+        {
+            return await _context.Usuarios.FindAsync(id);
+        }
+        public async Task AddAsync(Usuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdateAsync(Usuario usuario)
+        {
+            _context.Usuarios.Update(usuario);
+            await _context.SaveChangesAsync();
+        }
+        public async Task DeleteAsync(int id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+            if (usuario != null)
+            {
+                _context.Usuarios.Remove(usuario);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
