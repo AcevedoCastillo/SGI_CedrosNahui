8b8bdeafc81fef3ea9d75e4617d6ce0949d6afad
Kevin Grande <kevinbussines2020@gmail.com>
2025-05-25 01:01:06 -0600

    feat:  🎨 Get User List UI

---
+++ CedrosNahuizalquenos/CedrosNahuizalquenos.Client/Models/UsuarioDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace CedrosNahuizalquenos.Client.Models
+{
+    public class UsuarioDTO
+    {
+        public int UsuarioID { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string Correo { get; set; } = null!;
+        public string Contrasena { get; set; } = null!;
+        public int Rol { get; set; }
+        public bool Activo { get; set; }
+        public DateTime? FechaRegistro { get; set; }
+    }
+}
