ed54436b1877a953c3deec3e3abfcceccb5c8028
AcevedoCastillo <castilloacvd027@gmail.com>
2025-05-31 11:11:21 -0600

    :sparkles: finalizando el tercer reporte

---
+++ CedrosNahuizalquenos/CedrosNahuizalquenos.Client/Models/ReporteProductosDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace CedrosNahuizalquenos.Client.Models
+{
+    public class ReporteProductosDTO
+    {
+        public int ProductoID { get; set; }
+        public string? NombreProducto { get; set; }
+        public int TotalCantidadVendida { get; set; }
+        public decimal TotalVentas { get; set; }
+    }
+}
